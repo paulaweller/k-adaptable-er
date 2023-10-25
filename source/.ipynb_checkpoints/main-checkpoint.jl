508c996dac506b823de70c125df31b06a81cf68f
Paula Weller <paula.weller@aalto.fi>
2023-10-25 15:06:04 +0300

    make solutions output of instance run

--- source/.ipynb_checkpoints/main-checkpoint.jl
+++ source/.ipynb_checkpoints/main-checkpoint.jl
@@ -6,9 +6,9 @@ lim = 150.0         # runtime limit in seconds
 
 inst_read = read_instances_from_file("data/test_batch.txt")
 
-results1 = run_instance(k, inst_read[1], tlim=lim)
-#results2 = run_instance(k, inst_read[2], tlim=lim)
-#results3 = run_instance(k, inst_read[3], tlim=lim)
+results1, solutions1 = run_instance(k, inst_read[1], tlim=lim)
+#results2, solutions2 = run_instance(k, inst_read[2], tlim=lim)
+#results3, solutions3 = run_instance(k, inst_read[3], tlim=lim)
 
 
 result_data = DataFrame(results1)

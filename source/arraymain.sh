45712c31aa32f88a82ed755a5971879b9f6d870d
Paula Weller <paula.weller@aalto.fi>
2023-11-08 18:53:36 +0200

    run experiments

--- source/arraymain.sh
+++ source/arraymain.sh
@@ -4,8 +4,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --output=output/array_%A_%a.out
 #SBATCH --array=1-50
-
-export FILE="data_batch_6_10_0.1"
+export FILE="data_batch_6_20_0.3"
 # "data_batch_8_10_0.1" # "data_batch_8_10_0.3"
 # "data_batch_8_15_0.1" # "data_batch_8_15_0.3"
 # "data_batch_8_20_0.1" # "data_batch_8_20_0.3"

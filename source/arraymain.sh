7b57e2824fa0af01f97fa4913e685a8b2dbbaafc
Paula Weller <paula.weller@aalto.fi>
2023-11-13 13:34:16 +0200

    last results

--- source/arraymain.sh
+++ source/arraymain.sh
@@ -3,8 +3,7 @@
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=8
 #SBATCH --output=output/array_%A_%a.out
-#SBATCH --array=1-50
-#SBATCH --exclude=milan14
+#SBATCH --array=22,24,29,35,43,45
 
 # export FILE="data_batch_4_10_0.1" 
 # export FILE="data_batch_4_10_0.3"
@@ -25,9 +24,10 @@
 # export FILE="data_batch_8_15_0.1" 
 # export FILE="data_batch_8_15_0.3"
 # export FILE="data_batch_8_20_0.1" 
-# export FILE="data_batch_8_20_0.3"
+# 
+export FILE="data_batch_8_20_0.3"
 # k
-export K=3
+export K=2
 export L=$SLURM_ARRAY_TASK_ID
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 mkdir results/$FILE
@@ -35,7 +35,7 @@ mkdir results/$FILE/individual
 
 module load julia
 srun julia --project=. arraymain.jl $K $L $FILE
-awk '(NR == 1) || (FNR > 1)' results/$FILE/individual/results_${FILE}_k${K}_*.csv > results/$FILE/combined_results_${FILE}_k$K.csv
+#awk '(NR == 1) || (FNR > 1)' results/$FILE/individual/results_${FILE}_k${K}_*.csv > results/$FILE/combined_results_${FILE}_k$K.csv
 
 # rm results/data_batch_4_10_0.1/individual/results_data_batch_4_10_0.1_k1_*.csv
 ####### find . -type f -name results_data_batch_6_10_0.1_\* -exec rm {} \;
---
+++ source/combine.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --time=00:1:00
+#SBATCH --mem=1G
+
+# export FILE="data_batch_4_10_0.1" 
+# export FILE="data_batch_4_10_0.3"
+# export FILE="data_batch_4_15_0.1" 
+# 
+export FILE="data_batch_4_15_0.3"
+# export FILE="data_batch_4_20_0.1" 
+# export FILE="data_batch_4_20_0.3"
+# n = 6
+# export FILE="data_batch_6_10_0.1" 
+# export FILE="data_batch_6_10_0.3"
+# export FILE="data_batch_6_15_0.1" 
+# export FILE="data_batch_6_15_0.3"
+# export FILE="data_batch_6_20_0.1" 
+# export FILE="data_batch_6_20_0.3"
+# n = 8
+# export FILE="data_batch_8_10_0.1" 
+# export FILE="data_batch_8_10_0.3"
+# export FILE="data_batch_8_15_0.1" 
+# export FILE="data_batch_8_15_0.3"
+# export FILE="data_batch_8_20_0.1" 
+# export FILE="data_batch_8_20_0.3"
+# k
+export K=1
+awk '(NR == 1) || (FNR > 1)' results/$FILE/individual/results_${FILE}_k${K}_*.csv > results/$FILE/combined_results_${FILE}_k$K.csv

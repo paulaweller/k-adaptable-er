3df5ed5e7fe00c306bab85c653588dff20c70154
Paula Weller <paula@famweller.de>
2023-11-20 13:40:14 +0200

    process results, more plots

--- source/main.jl
+++ source/main.jl
@@ -7,7 +7,7 @@ allfile = "source/results/all_batches/combined_results_all_batches"
 no = [4,6,8]
 mo = [10,15,20]
 pco = [0.1, 0.3]
-ko = [5]
+ko = [2,3]
 
 # zetas, bbs, boxs = extract_evolutions(no, mo, pco, ko)
 # plot_evol(bbs, xlimits=[0,3600], relative=false, name="bb0.3", last = false)
@@ -22,11 +22,9 @@ for k in ko
         observables = Dict(
             :o_pb => o_pb, 
             :o_bb => o_bb, 
-            :o_box => o_box, 
-            :oo_pb => oo_pb, 
-            :oo_box => oo_box)
+            :o_box => o_box)
         observable_data = DataFrame(observables)
-        write_result_to_file("results/all_batches/observables_n$(no)_m$(mo)_pc$(pc)_k$(k).csv", observable_data)
+        write_result_to_file("source/results/all_batches/observables_n$(no)_m$(mo)_pc$(pc)_k$(k).csv", observable_data)
 
         observable_plot(o_pb,o_bb,o_box,oo_pb, oo_box,k,pc,rel=true)
         observable_plot(o_pb,o_bb,o_box,oo_pb, oo_box,k,pc,rel=false)
--- source/trysomething.jl
+++ source/trysomething.jl
@@ -1,12 +1,40 @@
-function tryit()
-    i = 0
-    g = 0
-    while i < 5
-        g = 2*i
-        i = i+1
-    end
+using DataFrames, CSV
 
-    return g
+function extract_results(no, mo, pco, ko)
+    results = DataFrame()
+    for n in no
+        for m in mo
+            for pc in pco
+                for k in ko
+                    # Specify the directory path
+                    directory_path = "source/results/data_batch_$(n)_$(m)_$(pc)"
+
+                    # Specify the common beginning of the filename
+                    common_prefix = "combined_results_data_batch_$(n)_$(m)_$(pc)_k$(k).csv"
+
+                    # Get a list of files in the directory that match the pattern
+                    files = filter(x -> occursin(common_prefix, x), readdir(directory_path))
+                    for file in files
+                        # get file content
+                        data = DataFrame(CSV.File(directory_path*"/"*file))
+
+                        # add columns for n, m, pc, k
+                        data[!,:n] .= n
+                        data[!,:m] .= m
+                        data[!,:pc] .= pc
+                        data[!, :k] .= k
+
+                        #merge with other data
+                        results = vcat(results, data)
+                    end
+                end
+            end
+        end
+    end
+    output = open("source/results/all_batches/combined_results_all_batches.csv", "w")
+    CSV.write(output, results)
+    close(output)
+    return results
 end
 
-tryit()
+extract_results([4,6,8],[10,15,20], [0.1, 0.3], [1,2,3,4,5])

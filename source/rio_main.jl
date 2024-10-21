c193d3a704b1b1c090cc49fe72d6e8c74bd54813
Paula Weller <paula@famweller.de>
2024-10-21 13:46:54 +0300

    correct filepath

--- source/rio_main.jl
+++ source/rio_main.jl
@@ -6,11 +6,11 @@ product = parse(String, ARGS[2]) # supply product
 lim = 7200.0         # runtime limit in seconds
 
 # supplies
-W_df = CSV.read("source/data/rio_supplies.csv", DataFrame)
+W_df = CSV.read("data/rio_supplies.csv", DataFrame)
 W = W_df[W_df[!,1].=="persons total", product][1]
 
 # demand bound per location
-D_df = CSV.read("source/data/rio_demand.csv", DataFrame)
+D_df = CSV.read("data/rio_demand.csv", DataFrame)
 max_pc_per_area = 0.3679
 D = D_df[D_df[!,1].!="Total", "Population"].*max_pc_per_area
 
@@ -18,7 +18,7 @@ D = D_df[D_df[!,1].!="Total", "Population"].*max_pc_per_area
 pc_overall = 0.1096
 
 # distances between supply and demand points
-dist_df = CSV.read("source/data/rio_dist.csv", DataFrame)
+dist_df = CSV.read("data/rio_dist.csv", DataFrame)
 dist = Array(dist_df[!,2:end])
 
 # empty set of locations
@@ -37,10 +37,10 @@ results, solutions = run_instance(k, instance, tlim=lim, return_solutions=save_x
 result_data = DataFrame(results)
 #io = open("source/results/rio/individual/results_sol_rio_k$(k).csv", "a")
 #close(io)
-write_result_to_file("source/results/rio/$(product)/results_$(product)_k$(k).csv", result_data)
+write_result_to_file("results/rio/$(product)/results_$(product)_k$(k).csv", result_data)
 
 if save_x
-     output = open("source/results/rio/$(product)/individual/sol_rio_$(product)_k$(k).txt", "w")
+     output = open("results/rio/$(product)/individual/sol_rio_$(product)_k$(k).txt", "w")
      CSV.write(output, solutions)
      close(output)
 end

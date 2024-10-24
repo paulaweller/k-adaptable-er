544b90d4b97013e1f528f58d62ef43222cb61433
Paula Weller <paula@famweller.de>
2024-10-24 16:02:36 +0300

    delete unneeded stuff

--- source/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:10:00
-#SBATCH --mem=1G
-#SBATCH --output=output.out
-
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
-module load julia
-julia --project=. generate_ins.jl
